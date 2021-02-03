31fdbca3fe4374cbb00c897058a6503c58319384
Janengethe <janengethew@gmail.com>
2021-02-03 20:08:44 +0000

    edited file 101

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -2,11 +2,11 @@
 #include <unistd.h>
 /**
  *main - Entry point
- *write - print a string
- *Return: return to main
+ *Return: Always 1
+ *
  */
 int main(void)
 {
-write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 58);
-return (0);
+write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+return (1);
 }

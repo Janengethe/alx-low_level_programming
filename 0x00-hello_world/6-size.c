b13f9c2debda73dfb94cc8e11da7bce4be7d771b
Janengethe <janengethew@gmail.com>
2021-02-03 19:49:41 +0000

    edited file 6

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -14,5 +14,6 @@ printf("Size of char: %zu byte\n", sizeof(charType));
 printf("Size of int: %zu bytes\n", sizeof(intType));
 printf("Size of long int: %zu bytes\n", sizeof(longType));
 printf("Size of long long int: %zu bytes\n", sizeof(longlongType));
-printf("Size of float: %zu bytes\n", sizeof(floatType)); return (0);
+printf("Size of float: %zu bytes\n", sizeof(floatType));
+return (0);
 }

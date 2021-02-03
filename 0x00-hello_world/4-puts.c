c02a668bb387b167cd400aa853f3e297d6347881
Janengethe <janengethew@gmail.com>
2021-02-03 20:05:22 +0000

    edited file 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 /**
- *main - entry point
- *Description - this is a hello,puts
- *Return: returns an integer
+ * main - Entry point
+ *
+ * Return: Always 0
  */
 int main(void)
 {
-puts("\"Programming is like building a multilingual puzzle\n");
+puts("\"Programming is like building a multilingual puzzle");
 return (0);
 }

8b1aebac26e9b0fb5084b208abae883a81edc3c5
Janengethe <janengethew@gmail.com>
2021-02-03 14:31:00 +0000

    edited file 4

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ *main - entry point
+ *Description - this is a hello,puts
+ *Return: returns an integer
+ */
+int main(void)
+{
+puts("\"Programming is like building a multilingual puzzle\n");
+return (0);
+}

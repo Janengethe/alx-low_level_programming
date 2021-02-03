b6b5a62f51277ad507d2340d778be2f7be625a25
Janengethe <janengethew@gmail.com>
2021-02-03 14:46:31 +0000

    edited file

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ *main - entry point
+ *Description - this uses printf
+ *Return: return an integer
+ */
+int main(void)
+{
+printf("with proper grammar, but the outcome is a piece of art,\n");
+return (0);
+}

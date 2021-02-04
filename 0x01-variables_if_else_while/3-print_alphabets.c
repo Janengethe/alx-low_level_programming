428c511f8bd2792d5d73af74f0e5dbf10767e794
Janengethe <janengethew@gmail.com>
2021-02-04 17:57:04 +0000

    edited file 3

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+/**
+ *main - entry point
+ *Return: is an integer
+ */
+int main(void)
+{
+char alphabet_lowercase, alphabet_uppercase, newline;
+alphabet_lowercase = 'a';
+alphabet_uppercase = 'A';
+newline = '\n';
+while (alphabet_lowercase <= 'z')
+{
+putchar(alphabet_lowercase);
+alphabet_lowercase++;
+}
+while (alphabet_uppercase <= 'Z')
+{
+putchar(alphabet_uppercase);
+alphabet_uppercase++;
+}
+putchar(newline);
+return (0);
+}

d0d582b11ad82411e808eece083d676410c96f98
Janengethe <janengethew@gmail.com>
2021-02-04 18:20:43 +0000

    edited file 4

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+/**
+ *main - entry point
+ *Return: returns 0
+ */
+int main(void)
+{
+char alphabet, newline;
+alphabet = 'a';
+newline = '\n';
+while (alphabet <= 'z')
+{
+if (alphabet == 'q' || alphabet == 'e')
+{
+alphabet++;
+}
+else
+{
+putchar (alphabet);
+alphabet++;
+}
+}
+putchar (newline);
+return (0);
+}

99bb558e1fa3ede406e9d1323df01885c650d9af
Janengethe <janengethew@gmail.com>
2021-02-04 19:53:03 +0000

    edited file 7

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ *main - entry point
+ *Return: returns 0
+ */
+int main(void)
+{
+char alphabet, newline;
+alphabet = 'z';
+newline = '\n';
+while (alphabet >= 'a')
+{
+putchar (alphabet);
+alphabet--;
+}
+putchar (newline);
+return (0);
+}

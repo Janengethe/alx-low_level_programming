562a2da4a7ade021c13ee832044db4384c614b43
Janengethe <janengethew@gmail.com>
2021-02-04 16:50:35 +0000

    edited file 2

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ *main - entry point
+ *Return: returns 0
+ */
+int main(void)
+{
+char alphabet, newline;
+alphabet = 'a';
+newline = '\n';
+while (alphabet <= "z")
+{
+putchar (alphabet);
+alphabet++;
+}
+putchar (newline);
+return (0);
+}

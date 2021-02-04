701815bf04679bd93adfd0d7bab47993f9f499be
Janengethe <janengethew@gmail.com>
2021-02-04 19:13:49 +0000

    edited file6

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ *main - entry point
+ *Return: is an integer
+ */
+int main(void)
+{
+int number, newline;
+number = 0;
+newline = '\n'
+while (number < 10)
+{
+putchar(number);
+number++;
+}
+putchar(newline);
+return (0);
+}

ca9ce108358726f904a5d317d523ac88799207b3
Janengethe <janengethew@gmail.com>
2021-02-05 10:59:56 +0000

    edited file 9

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ *main - print combination of all single-digit numbers using putchar
+ *Return: always return 0 (success)
+ */
+int main(void)
+{
+int n = '0';
+while (n <= '9')
+{
+putchar(n);
+if (n != '9')
+{
+putchar(',');
+putchar(' ');
+}
+n++;
+}
+putchar('\n');
+return (0);
+}

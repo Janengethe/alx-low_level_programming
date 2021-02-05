8e968ee311c39463e3cacb03a77d77b53bd9af76
Janengethe <janengethew@gmail.com>
2021-02-05 09:51:07 +0000

    edited file 8

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -5,17 +5,18 @@
  */
 int main(void)
 {
-char a = 0;
-while (a < 10)
+char a;
+a = '0';
+while (a <= 9)
 {
 putchar(a + '0');
-a++
+a++;
 }
 a = 'a';
 while (a <= 'f')
 {
 putchar(a);
-a++
+a++;
 }
 putchar('\n');
 return (0);

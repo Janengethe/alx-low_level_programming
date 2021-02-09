1f9cfe555c356ac4a79b765689b7871b758c9a2e
Janengethe <janengethew@gmail.com>
2021-02-09 06:23:39 +0000

    edited file holberton.h

--- 0x02-functions_nested_loops/holberton.h
+++ 0x02-functions_nested_loops/holberton.h
@@ -1,8 +1,9 @@
+#ifndef _HOLBERTON_H_
+#define _HOLBERTON_H_
 #include <unistd.h>
 /**
  * _putchar - writes the character c to stdout
- * @c: The character to print
- *
+ *@c: The character to print
  * Return: On success 1.
  * On error, -1 is returned, and errno is set appropriately.
  */
@@ -48,6 +49,7 @@ return;
 /**
  *_islower - check for lowercase character
  *Return: always 0
+ *@c : this is a parameter
  */
 int _islower(int c)
 {
@@ -61,3 +63,4 @@ return (0);
 }
 return (0);
 }
+#endif

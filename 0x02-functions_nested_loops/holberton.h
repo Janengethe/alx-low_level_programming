86cbcebb7a7d237adf0172abb312ebce34b07f32
Janengethe <janengethew@gmail.com>
2021-02-09 07:20:15 +0000

    edited .h

--- 0x02-functions_nested_loops/holberton.h
+++ 0x02-functions_nested_loops/holberton.h
@@ -1,66 +1,11 @@
 #ifndef _HOLBERTON_H_
 #define _HOLBERTON_H_
-#include <unistd.h>
 /**
  * _putchar - writes the character c to stdout
- *@c: The character to print
+ * @c: The character to print
+ *
  * Return: On success 1.
  * On error, -1 is returned, and errno is set appropriately.
  */
 int _putchar(char c)
-{
-return (write(1, &c, 1));
-}
-/**
- *print_alphabet - print alphabet in lowercase
- *Return: returns void
- */
-void print_alphabet(void)
-{
-char n = 'a';
-while (n <= 'z')
-{
-_putchar (n);
-n++;
-}
-_putchar ('\n');
-return;
-}
-/**
- *print_alphabet_x10 - prints alphabets 10x
- *Return: return void
- */
-void print_alphabet_x10(void)
-{
-int n = 0, a;
-while (n < 10)
-{
-a = 'a';
-while (a <= 'z')
-{
-_putchar (a);
-a++;
-}
-_putchar ('\n');
-n++;
-}
-return;
-}
-/**
- *_islower - check for lowercase character
- *Return: always 0
- *@c : this is a parameter
- */
-int _islower(int c)
-{
-if (c >= 'a' && c <= 'z')
-{
-return (1);
-}
-else
-{
-return (0);
-}
-return (0);
-}
 #endif

5d2e2f320a52eb23781f33bb9aa7822690cf0094
Janengethe <janengethew@gmail.com>
2021-02-09 10:23:35 +0000

    edited files 9-11

--- 0x02-functions_nested_loops/holberton.h
+++ 0x02-functions_nested_loops/holberton.h
@@ -51,4 +51,25 @@ int print_last_digit(int);
  *Return: return 0
  */
 void jack_bauer(void);
+/**
+ * times_table - print multiplication table
+ */
+void times_table(void);
+/**
+ * add - add two integers
+ * @a: first integer
+ * @b: second integer
+ * Return: sum
+ */
+int add(int a, int b);
+/**
+ * print_to_98 - print numbers n to 98
+ * @n: integer
+ */
+void print_to_98(int n);
+/**
+ * print_times_table - print multiplication table up to n
+ * @n: integer argument
+ */
+void print_times_table(int n);
 #endif

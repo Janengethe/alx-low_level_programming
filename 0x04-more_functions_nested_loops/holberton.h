5d8d56df6efbf0da665bd30f8aa4c70d10bec407
Janengethe <janengethew@gmail.com>
2021-02-11 08:07:02 +0000

    edited holberton.h

---
+++ 0x04-more_functions_nested_loops/holberton.h
@@ -0,0 +1,27 @@
+#ifndef _HOLBERTON_H_
+#define _HOLBERTON_H_
+
+/**
+ * _putchar - writes the character c to stdout
+ * @c: The character to print
+ *
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+int _putchar(char c);
+
+/**
+ *_isupper - checks for uppercase characters
+ *@c: integer to print
+ *Return: return 1 if uppercase and 0 if otherwise
+ */
+int _isupper(int c);
+
+/**
+ *_isdigit - checks for a digit
+ *@c: integer to print
+ *Return: return 1 if a digit0 if otherwise
+ */
+int _isdigit(int c);
+
+#endif /* _HOLBERTON_H_ */

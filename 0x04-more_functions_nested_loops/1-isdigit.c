e201278e30b1952ee9aab7a3a9c1a872c4c327d2
Janengethe <janengethew@gmail.com>
2021-02-11 08:21:14 +0000

    edited file 1-isdigit.c

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,18 @@
+#include "holberton.h"
+
+/**
+ *_isdigit - checks for a digit
+ *@c: integer to print
+ *Return: return 1 if a digit0 if otherwise
+ */
+int _isdigit(int c)
+{
+if (c >= '0' && c <= '9')
+{
+return (1);
+}
+else
+{
+return (0);
+}
+}

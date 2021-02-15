448da14c85015d84d6427290e5d3d0f7ed34845b
Janengethe <janengethew@gmail.com>
2021-02-15 12:19:40 +0000

    created README.md and empty files

---
+++ 0x05-pointers_arrays_strings/holberton.h
@@ -0,0 +1,14 @@
+#ifndef _HOLBERTON_H_
+#define _HOLBERTON_H_
+
+#include <unistd.h>
+/**
+ * _putchar - writes the character c to stdout
+ * @c: The character to print
+ *
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+int _putchar(char c);
+
+#endif /* _HOLBERTON_H_ */

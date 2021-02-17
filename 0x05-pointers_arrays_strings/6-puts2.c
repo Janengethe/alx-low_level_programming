482b1608ffd8d7a577829bfff13b1fc530b7c168
Janengethe <janengethew@gmail.com>
2021-02-17 11:13:11 +0000

    used for loop

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -24,11 +24,8 @@ return (length);
  */
 void puts2(char *str)
 {
-int i = 0;
-while (str[i] != '\0')
-{
-_putchar (str[i]);
-i += 2;
-}
-_putchar ('\n');
+int i;
+for (i = 0; str[i] != '\0' && i < _strlen(str); i += 2)
+_putchar(str[i]);
+_putchar('\n');
 }

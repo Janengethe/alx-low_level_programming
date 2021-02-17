712c5df54ff3b909b7dc48347e11d30f1ebf160c
Janengethe <janengethew@gmail.com>
2021-02-17 10:11:06 +0000

    added a strlen

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -1,16 +1,34 @@
 #include "holberton.h"
 
 /**
+ * _strlen - returns the length of a string
+ * @s: string
+ * Return: length
+ */
+
+int _strlen(char *s)
+{
+int length = 0;
+while (*s != '\0')
+{
+length++;
+s++;
+}
+return (length);
+}
+
+/**
  *puts2 - prints the characters of a string
  *@str: character to print
  *Return: nothing
  */
 void puts2(char *str)
 {
-while (*str != '\0')
+int i;
+while (str[i] != '\0')
 {
-_putchar (*str);
-str += 2;
+_putchar (str[i]);
+i += 2;
 }
 _putchar ('\n');
 }

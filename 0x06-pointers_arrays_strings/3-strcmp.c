4cb640eb36c1138715c61d8a83a60f921de1abe3
Janengethe <janengethew@gmail.com>
2021-02-18 12:51:18 +0000

    wrote a function to compare strings

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,20 @@
+#include "holberton.h"
+
+/**
+ *_strcmp - compares strings
+ *@s1: string character
+ *@s2: string character
+ *Return: a pointer to string
+ */
+int _strcmp(char *s1, char *s2)
+{
+int i = 0;
+while (s1[i] != '\0' || s2[i] != '\0')
+{
+if (s1[i] != s2[i])
+return (s1[i] - s2[i]);
+s1[i]++;
+s2[i]++;
+}
+return (0);
+}

69eb42c411d33936f3a427ce520d2a4fdb1689e9
Janengethe <janengethew@gmail.com>
2021-03-02 12:31:45 +0000

    initialized for the NULL character

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -14,6 +14,12 @@ char *str_concat(char *s1, char *s2)
 char *concat;
 int i1, i2, length1, length2;
 
+if (s1 == NULL)
+s1 = "";
+
+if (s2 == NULL)
+s2 = "";
+
 length1 = 0;
 while (*(s1 + length1))
 length1++;

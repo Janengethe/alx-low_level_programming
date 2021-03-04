0f65e520cc719c7575075efd7cffee25a40c67fc
Janengethe <janengethew@gmail.com>
2021-03-04 18:10:57 +0000

    freed memory from ptr before introducing the program

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -12,10 +12,11 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 {
 void *p;
 unsigned int i;
+if (ptr == NULL)
+free(ptr);
 
 if (new_size == old_size)
 return (ptr);
-
 if (ptr == NULL)
 {
 

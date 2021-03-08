d60be13f414c1b99616d331662a1b872c056e7aa
Janengethe <janengethew@gmail.com>
2021-03-08 13:40:45 +0000

    initial commit

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,15 @@
+#include "dog.h"
+
+/**
+ *init_dog - initializes a variable of type struct dog.
+ *@d: variable of struct dog.
+ *@name: element
+ *@age: element
+ *@owner: element
+ */
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+d->name = name;
+d->age = age;
+d->owner = owner;
+}

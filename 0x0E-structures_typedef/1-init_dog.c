8cda9ce2e49006416fe43000a3d247d5993321aa
Janengethe <janengethew@gmail.com>
2021-03-08 13:46:45 +0000

    checked if d is initialized correctly

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -1,3 +1,4 @@
+#include <stdio.h>
 #include "dog.h"
 
 /**
@@ -9,6 +10,8 @@
  */
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
+if (d == NULL) /* validate if d is initialized corrctly */
+return;
 d->name = name;
 d->age = age;
 d->owner = owner;

cda00e00e3cce359b0f5f289aed51728e07327ea
Janengethe <janengethew@gmail.com>
2021-03-10 09:27:59 +0000

    include guarded

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,6 +1,6 @@
 #include "function_pointers.h"
 #include <stdio.h>
-
+#include <stdlib.h>
 /**
  *print_name - prints a name
  *@name: name to print
@@ -8,6 +8,6 @@
  */
 void print_name(char *name, void (*f)(char *))
 {
-if (name != NULL && f != NULL)
+if (name != NULL)
 f(name);
 }

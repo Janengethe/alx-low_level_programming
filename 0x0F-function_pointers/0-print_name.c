0ba4ecee978447cb759d2b871f5e680324566dfc
Janengethe <janengethew@gmail.com>
2021-03-10 09:18:52 +0000

    initial commit

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,13 @@
+#include "function_pointers.h"
+#include <stdio.h>
+
+/**
+ *print_name - prints a name
+ *@name: name to print
+ *@f: function pointer
+ */
+void print_name(char *name, void (*f)(char *))
+{
+if (name != NULL)
+f(name);
+}

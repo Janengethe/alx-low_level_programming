9ba1467e047dc29e692e2f8307e19b0abfe6494d
Janengethe <janengethew@gmail.com>
2021-03-11 10:47:07 +0000

    initialized i to 1

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -13,16 +13,19 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 va_list valist;
 unsigned int i;
 
+if (n > 0)
+{
 va_start(valist, n);
 
-for (i = 0; i < n; i++)
+for (i = 1; i <= n; i++)
 {
 printf("%d", va_arg(valist, int));
 
-if (i < n && separator != NULL)
+if (i != n && separator != NULL)
 printf("%s", separator);
 }
 
 va_end(valist);
+}
 printf("\n");
 }

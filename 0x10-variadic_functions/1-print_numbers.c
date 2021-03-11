778182722a766d9ec79d1cc5fa8bb68d8e6645e3
Janengethe <janengethew@gmail.com>
2021-03-11 10:33:53 +0000

    checked if i = n

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -12,19 +12,16 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 va_list valist;
 unsigned int i;
 
-if (n > 0)
-{
 va_start(valist, n);
 
 for (i = 0; i < n; i++)
 {
-printf("%d", va_arg(valist, unsigned int));
+printf("%d", va_arg(valist, int));
 
-if (separator != NULL)
+if (i != n && separator != NULL)
 printf("%s", separator);
 }
 
 va_end(valist);
-}
 printf("\n");
 }

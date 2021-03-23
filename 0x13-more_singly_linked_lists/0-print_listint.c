a9c08fe89e0d2f3b50c7d44fe5ba3829ee2a250d
Janengethe <janengethew@gmail.com>
2021-03-23 12:51:06 +0000

    incremented the nodes number

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -1,18 +1,21 @@
 #include "lists.h"
 
 /**
- *print_listint - prints all the elements of a list
- *@h:pointer to elements
- *Return: number of nodes
+ * print_listint - prints all elements of a list
+ * @h: list
+ * Return: number of nodes
  */
 size_t print_listint(const listint_t *h)
 {
-size_t node_num = 0;
+size_t num_nodes = 0;
+
 while (h != NULL)
 {
 printf("%d\n", h->n);
-node_num++;
+
+num_nodes++;
 h = h->next;
 }
-return (node_num);
+
+return (num_nodes);
 }

800ee5f829e4c4bb3f02bcdc378b7aa80e683dbf
Janengethe <janengethew@gmail.com>
2021-03-29 16:18:03 +0000

    edited file

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -8,19 +8,17 @@
  */
 int create_file(const char *filename, char *text_content)
 {
-int fd, fwrite, i;
+int fd, fwrite, i = 0;
 
 if (filename == NULL)
 return (-1);
 
-if (text_content == NULL)
-return (1);
-
 fd = open(filename, O_RDWR | O_CREAT | O_TRUNC, 0600);
 if (fd == -1)
 return (-1);
-
-for (i = 0; text_content[i] != '\0'; i++)
+if (text_content == NULL)
+return (1);
+for (; text_content[i] != '\0'; i++)
 ;
 
 fwrite = write(fd, text_content, i);

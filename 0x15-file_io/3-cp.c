efccc578d9b2d4c3d516cb6df758b7b2fc066284
Janengethe <janengethew@gmail.com>
2021-03-31 08:37:30 +0000

    edited file

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -1,6 +1,33 @@
 #include "holberton.h"
 
 /**
+ * __exit - prints error messages and exits with exit value
+ * @error: num is either exit value or file descriptor
+ * @s: str is a name, either of the two filenames
+ * @fd: file descriptor
+ * Return: 0 on success
+ **/
+int __exit(int error, char *s, int fd)
+{
+switch (error)
+{
+case 97:
+dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
+exit(error);
+case 98:
+dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", s);
+exit(error);
+case 99:
+dprintf(STDERR_FILENO, "Error: Can't write to %s\n", s);
+exit(error);
+case 100:
+dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd);
+exit(error);
+default:
+return (0);
+}
+}
+/**
  *main - print to command line
  *@argc: arguments
  *@argv: array of argc
@@ -8,40 +35,31 @@
  */
 int main(int argc, char *argv[])
 {
-int file_from, file_to, close1, close2, n;
+int file_from, file_to, n_read, n_write;
 char buffer[BUFSIZ];
+
 if (argc != 3)
-{
-dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
-exit(97);
-}
+__exit(97, NULL, 0);
+
 file_from = open(argv[1], O_RDONLY);
-if (file_from < 0)
+if (file_from == -1)
+__exit(98, argv[1], 0);
+
+file_to = open(argv[2], O_CREAT | O_TRUNC | O_WRONLY, 0664);
+if (file_to == -1)
+__exit(99, argv[2], 0);
+
+while ((n_read = read(file_from, buffer, BUFSIZ)) != 0)
 {
-dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
-exit(98);
+if (n_read == -1)
+__exit(98, argv[1], 0);
+
+n_write = write(file_to, buffer, n_read);
+if (n_write == -1)
+__exit(99, argv[2], 0);
 }
-file_to = open(argv[2], O_CREAT | O_RDWR | O_TRUNC, 0664);
-while ((n = read(file_from, buffer, BUFSIZ)) > 0)
-{
-if (write(file_from, buffer, n) != n)
-{
-dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
-close(file_from);
-exit(99);
-}
-}
-if (n < 0)
-{
-dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
-exit(98);
-}
-close1 = close(file_from);
-close2 = close(file_to);
-if (close1 < 0 || close2 < 0)
-{
-dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", file_from);
-exit(100);
-}
+
+close(file_to) == -1 ? (__exit(100, NULL, file_to)) : close(file_to);
+close(file_from) == -1 ? (__exit(100, NULL, file_from)) : close(file_from);
 return (0);
 }

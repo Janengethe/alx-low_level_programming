3dabd811587cd257c97a2b8aa5a13c95c00f096b
Janengethe <janengethew@gmail.com>
2021-07-03 01:01:48 -0700

    cheked for betty

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <unistd.h>
+
 /**
  *main - Entry point
  *Return: Always 1
@@ -7,6 +8,6 @@
  */
 int main(void)
 {
-write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
-return (1);
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+	return (1);
 }

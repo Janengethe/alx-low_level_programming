b7d4a0808a6f1b0f70175cfad6389a03d755278d
Janengethe <janengethew@gmail.com>
2022-01-17 09:41:25 -0800

    initial commit

---
+++ 0x1E-search_algorithms/README.md
@@ -0,0 +1,2 @@
+## 0x1E. C - Search Algorithms
+> Search algorithm, time complexity and space complexity
---
+++ 0x1E-search_algorithms/search_algos.h
@@ -0,0 +1,7 @@
+#ifndef SEARCH_ALGOS
+#define SEARCH_ALGOS
+
+int linear_search(int *array, size_t size, int value);
+int binary_search(int *array, size_t size, int value);
+
+#endif

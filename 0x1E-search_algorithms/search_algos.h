bd8f5b1ed6e9e3840dc749ada0316d085c58f47d
Janengethe <janengethew@gmail.com>
2022-01-17 11:08:33 -0800

    task 0

--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -1,6 +1,10 @@
 #ifndef SEARCH_ALGOS
 #define SEARCH_ALGOS
 
+#include <stddef.h>
+#include <stdio.h>
+#include <stdlib.h>
+
 int linear_search(int *array, size_t size, int value);
 int binary_search(int *array, size_t size, int value);
 

4241949d65c3e56110567f11fd004ca04f8cf25f
Janengethe <janengethew@gmail.com>
2022-01-17 11:14:49 -0800

    tasks

--- 0x1E-search_algorithms/0-linear.c
+++ 0x1E-search_algorithms/0-linear.c
@@ -11,7 +11,7 @@ int linear_search(int *array, size_t size, int value)
 	size_t i;
 
 	if (!array)
-		return(-1);
+		return (-1);
 
 	i = 0;
 	while (i < size)
--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -7,5 +7,6 @@
 
 int linear_search(int *array, size_t size, int value);
 int binary_search(int *array, size_t size, int value);
+void print_arr(int *array, size_t l, size_t r);
 
 #endif
